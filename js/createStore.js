010268845e17b4be00e58178df1e240d2ea4bf46
github username <github email address>
2020-01-24 16:04:37 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,40 @@
-let state;
-
+function createStore() {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  }
+ 
+  return {
+    dispatch,
+    getState
+  };
+};
+ 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
-
+ 
     default:
       return state;
   }
 };
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
-
+ 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
-
-dispatch({ type: '@@INIT' })
+ 
+let store = createStore();
+store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+ 
+button.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
